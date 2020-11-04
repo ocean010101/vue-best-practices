bcdb5d6d731173ec64539d76e58a4fb6eed910c7
ocean010101 <yangyang01.xu@outlook.com>
2020-11-04 10:21:44 +0800

    mock

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,5 @@
 import { getToken, setToken, removeToken } from "@/utils/auth";
+import {login, getInfo} from '@/api/user';
 const state = {
   token: getToken(),
   roles: []
@@ -15,28 +16,37 @@ const mutations = {
 const actions = {
   // user login
   login({ commit }, userInfo) {
-    const { username } = userInfo;
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (username === "admin" || username === "editor") {
-          commit("SET_TOKEN", username);
-          setToken(username);
-          resolve();
-        } else {
-          reject("用户名、密码错误");
-        }
-      }, 1000);
+    // 调用并处理结果，错误处理已拦截无需处理
+    return login(userInfo).then((res) => {
+      commit("SET_TOKEN", res.data);
+      setToken(res.data);
     });
+    // const { username } = userInfo;
+    // return new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     if (username === "admin" || username === "editor") {
+    //       commit("SET_TOKEN", username);
+    //       setToken(username);
+    //       resolve();
+    //     } else {
+    //       reject("用户名、密码错误");
+    //     }
+    //   }, 1000);
+    // });
   },
   // get user info
   getInfo({ commit, state }) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        const roles = state.token === 'admin' ? ['admin'] : ['editor']
-        commit("SET_ROLES", roles);
-        resolve({ roles });
-      }, 1000);
-    });
+    return getInfo(state.token).then(({data: roles}) => {
+      commit("SET_ROLES", roles);
+      return {roles}
+    })
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     const roles = state.token === 'admin' ? ['admin'] : ['editor']
+    //     commit("SET_ROLES", roles);
+    //     resolve({ roles });
+    //   }, 1000);
+    // });
   },
   // remove token
   resetToken({ commit }) {
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 // resolve定义一个绝对路径获取函数
 const path = require('path')
+const bodyParser = require("body-parser");
+
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
@@ -10,6 +12,44 @@ module.exports = {
   publicPath: '/best-practice', // 部署应用包时的基本 URL
   devServer: {
     port: port,
+    before: app => {
+      // node服务器代码 基于express
+      // bodyPaser用来解析post请求中的json数据
+      app.use(bodyParser.json());
+      // app.use(
+      //   bodyParser.urlencoded({
+      //     extended: true
+      //   })
+      // );
+      //登录接口声明
+      app.post("/dev-api/user/login", (req, res) => {
+        const { username } = req.body;
+
+        if (username === "admin" || username === "editor") {
+          res.json({
+            code: 1,
+            data: username
+          });
+        } else {
+          res.json({
+            code: 10204,
+            message: "用户名或密码错误"
+          });
+        }
+      });
+
+      app.get("/dev-api/user/info", (req, res) => {
+        // 从请求头中获取令牌
+        // adfasdfkadf; ja;kdfj;akdfjakdsfj;akjdf; akjdf;kalsjf;ajf
+        // 令牌头         令牌体                     哈希
+        // 加密算法        用户信息；有效期          
+        const roles = req.headers['x-token'] === "admin" ? ["admin"] : ["editor"];
+        res.json({
+          code: 1,
+          data: roles
+        });
+      });
+    }
   },
   configureWebpack: {
     // 向index.html注入标题
